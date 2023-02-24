227833bcf17d3c9c675fa7a89ea8505802d465d0
vjodo <senchikhin@gmail.com>
2023-02-24 18:25:32 +0300

    6. Checkstyle [#4123]

--- src/main/java/ru/job4j/Main.java
+++ src/main/java/ru/job4j/Main.java
@@ -3,6 +3,6 @@ package ru.job4j;
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("привет, мир");
+        System.out.println("Hello, world!");
     }
 }
--- src/main/java/ru/job4j/StudentInfo.java
+++ src/main/java/ru/job4j/StudentInfo.java
@@ -1,7 +1,7 @@
 package ru.job4j;
 
 public class StudentInfo {
-    public static void main (String[] args) {
+    public static void main(String[] args) {
         System.out.println("Sergey Senchikhin");
         System.out.println("09.10.1984");
     }
