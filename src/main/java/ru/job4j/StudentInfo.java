05581b04bb9e4726ef1cb5b6e7bde85c909ae2da
vjodo <senchikhin@gmail.com>
2023-02-24 17:35:15 +0300

    removed .idea folder

---
+++ src/main/java/ru/job4j/StudentInfo.java
@@ -0,0 +1,8 @@
+package ru.job4j;
+
+public class StudentInfo {
+    public static void main (String[] args) {
+        System.out.println("Sergey Senchikhin");
+        System.out.println("09.10.1984");
+    }
+}
