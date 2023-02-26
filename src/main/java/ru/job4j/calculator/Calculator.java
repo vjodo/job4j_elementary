e85cc647bb215813c44b41bd1995fdf17d57057a
vjodo <senchikhin@gmail.com>
2023-02-26 12:35:04 +0300

    4.2. Результат работы метода [#232622]

--- src/main/java/ru/job4j/calculator/Calculator.java
+++ src/main/java/ru/job4j/calculator/Calculator.java
@@ -4,6 +4,7 @@ public class Calculator {
     public static void main(String[] args) {
         int one = 1;
         int two = 2;
+        one = 3;
         int onePlusTwo = one + two;
         System.out.println(onePlusTwo);
 
