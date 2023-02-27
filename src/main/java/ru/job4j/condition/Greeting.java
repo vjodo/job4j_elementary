380f21c14ceff39be1eb4b48da3ae94b47081355
vjodo <senchikhin@gmail.com>
2023-02-27 03:13:05 +0300

    9.1. String [#237407]

---
+++ src/main/java/ru/job4j/condition/Greeting.java
@@ -0,0 +1,11 @@
+package ru.job4j.condition;
+
+public class Greeting {
+    public static void main(String[] args) {
+        String idea = "I like Java!";
+        idea += " But I am a newbie. ";
+        int year = 2023;
+        idea += year;
+        System.out.println(idea);
+    }
+}
