b037003c0d55b7024d7b349ec69948d000a6fa3e
vjodo <senchikhin@gmail.com>
2023-11-14 22:35:41 +0300

    first commit

---
+++ src/main/java/ru/job4j/Test.java
@@ -0,0 +1,14 @@
+package ru.job4j;
+
+public class Test {
+
+    public static void main(String[] args) {
+        int i = 100;
+        ++i;
+       // System.out.println("Use increment: " + i++);
+        System.out.println("After increment: " + i);
+        System.out.println("Use decrement: " + i--);
+        System.out.println("After decrement: " + i);
+
+    }
+}
